710439e5aae189feb839d254ceed1306ee6180e6
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2021-07-04 23:12:28 +0700

    Many To Many Relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,21 @@ import { AppService } from './app.service';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 import { EventsModule } from './events/events.module';
+import { SchoolModule } from './school/school.module';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-    load: [ormConfig],
-    expandVariables: true
-  }),  
-  TypeOrmModule.forRootAsync({
-    useFactory: process.env.NODE_ENV !== 'production' 
-      ? ormConfig : ormConfigProd
-  }),
-  EventsModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true
+    }),  
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV !== 'production' 
+        ? ormConfig : ormConfigProd
+    }),
+    EventsModule,
+    SchoolModule
   ],
   controllers: [AppController],
   providers: [{

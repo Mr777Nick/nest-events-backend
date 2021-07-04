444a2e7f3f3b39899ee2be26269dcb648bdc1579
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2021-07-04 22:43:09 +0700

    Loading Related Entities

--- src/events/attendee.entity.ts
+++ src/events/attendee.entity.ts
@@ -12,8 +12,6 @@ export class Attendee {
     @ManyToOne(() => Event, (event) => event.attendees, {
         nullable: false
     })
-    @JoinColumn({
-        name: 'event_id'
-    })
+    @JoinColumn()
     event: Event;
 }

44f482ab280ce3d5f198b3f0e7f942ee50919a91
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2021-08-05 00:55:59 +0700

    JWT - Generating Token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,24 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 import { LocalStategy } from "./local.strategy";
 import { User } from "./user.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [LocalStategy],
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtModule.registerAsync({
+            useFactory: () => ({
+                secret: process.env.AUTH_SECRET,
+                signOptions: {
+                    expiresIn: '60m'
+                }
+            })
+        })
+    ],
+    providers: [LocalStategy, AuthService],
     controllers: [AuthController]
 })
 export class AuthModule {}

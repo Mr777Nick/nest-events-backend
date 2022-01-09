be43ef82478c52ef49ed8598b1f7b5d2181eb73a
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2022-01-10 01:44:15 +0700

    Introduction to E2E Testing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.NODE_ENV)
   const app = await NestFactory.create(
     AppModule,
     // {
